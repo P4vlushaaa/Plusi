838fe2fede9dd3895e51b2d85c5b0792aec1c9fb
P4vlushaaa <112944483+P4vlushaaa@users.noreply.github.com>
2023-06-07 22:26:42 +0300

    Delete OP5 directory

--- OP5/Pair.cpp
+++
@@ -1,7 +0,0 @@
-#include "Pair.h"
-
-template<class Type>
-Type Pair::findMin(Type a, Type b) {
-    if (a < b) return a;
-    return b;
-}
--- OP5/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "App.h"
-
-int main() {
-    return ((new App())->start());
-}
